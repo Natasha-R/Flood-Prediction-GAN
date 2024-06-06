2fe492613ee8a9c918f759ed661ce3b8fcdd304d
Natasha <12715753+Natasha-R@users.noreply.github.com>
2024-06-06 14:52:20 +0200

    Add utils.py

---
+++ utils.py
@@ -0,0 +1,96 @@
+import torch
+from torch import nn
+from datetime import datetime
+
+def initialise_weights(m):
+    classname = m.__class__.__name__
+    if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+        if hasattr(m, 'bias') and m.bias is not None:
+            nn.init.constant_(m.bias.data, 0.0)
+    elif classname.find('BatchNorm2d') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0.0)
+        
+def get_buffer_image(image, images_buffer):
+    image = image.detach()
+    if len(images_buffer) < 50: # if the buffer is not yet full, always append the new image AND return it
+        images_buffer.append(image.cpu())
+        return image
+    else:
+        p = random.uniform(0, 1)
+        if p > 0.5: # 50% chance the new image is stored and a random old image is returned
+            index = random.randint(0, 50 - 1)
+            old_image = images_buffer[index].clone()
+            images_buffer[index] = image.cpu()
+            return old_image.to(device)
+        else: # 50% chance the new image is not stored and the new image is returned
+            return image
+        
+def define_lambda_rule(num_epochs):
+    def lambda_rule(epoch):
+        learning_rate = 1.0 - max(0, epoch + 1 - (num_epochs / 2)) / float((num_epochs/2) + 1)
+        return learning_rate
+    return lambda_rule
+        
+def print_setup(continue_training, num_epochs, starting_epoch, not_input_topography, dataset_subset, dataset_dem, resize, crop, save_model_interval, save_images_interval):
+        print(f"\n{'Continuing' if continue_training is True else 'Beginning'} training:")
+        print(f"{num_epochs} epochs")
+        print(f"Starting from epoch {starting_epoch}")
+        print(f"Additional topographical factors will {'NOT ' if not_input_topography else ''}be input to the model")
+        print(f"Dataset: '{dataset_subset}' '{dataset_dem} DEM'")
+        print(f"Data resized to {resize} pixels with {crop} crops")
+        print(f"Models saved every {save_model_interval} epochs")
+        print(f"Sample generator output images saved every {save_images_interval} epochs\n")
+        
+def create_path(save, model, data_path, split, dataset_subset, dataset_dem, not_input_topography, resize, crop, epoch):
+    if save=="image":
+        file_type = ".png"
+    elif save=="model":
+        file_type = ".pth.tar"
+    else:
+        raise NotImplementedError("Path must relate to either images or a model")
+    path = f"{data_path}/data/{save}s/{model}_topography{not not_input_topography}_{split}_{dataset_subset}_{dataset_dem}DEM_resize{resize}_crop{crop}_epoch{epoch}_{str(datetime.now())[:-7].replace(' ', '-').replace(':', '-')}{file_type}"
+    return path
+
+def initialise_loss_storage(model, overall):
+    pre_string = ""
+    if overall:
+        pre_string = "all_"
+        
+    if model.lower()=="pix2pix":
+        return {f"{pre_string}losses_discriminator_real":[],
+                f"{pre_string}losses_discriminator_synthetic":[],
+                f"{pre_string}losses_generator_synthetic":[],
+                f"{pre_string}l1_losses_generator_synthetic":[]}
+    
+    elif model.lower()=="cyclegan":
+        return {f"{pre_string}losses_generator_post":[],
+                f"{pre_string}losses_generator_pre":[],
+                f"{pre_string}losses_pre_to_post_cycle":[],
+                f"{pre_string}losses_post_to_pre_cycle":[],
+                f"{pre_string}losses_discriminator_pre_real":[],
+                f"{pre_string}losses_discriminator_post_real":[],
+                f"{pre_string}losses_discriminator_pre_synthetic":[],
+                f"{pre_string}losses_discriminator_post_synthetic":[]}
+    
+    elif model.lower()=="attentiongan":
+        None
+        
+    else:
+        raise NotImplementedError("Model must be one of: Pix2Pix, CycleGAN or AttentionGAN")
+        
+def print_losses(model, epoch, all_losses):
+    
+    if model.lower()=="pix2pix":
+        print(f"{epoch=} | Discriminator real loss = {all_losses['all_losses_discriminator_real'][-1]:.2f} | Discriminator synthetic loss = {all_losses['all_losses_discriminator_synthetic'][-1]:.2f} | Generator synthetic loss = {all_losses['all_losses_generator_synthetic'][-1]:.2f} | L1 Generator loss = {all_losses['all_l1_losses_generator_synthetic'][-1]:.2f}")
+    
+    elif model.lower()=="cyclegan":
+        print(f"{epoch=} | Generator post image loss = {all_losses['all_losses_generator_post'][-1]:.2f} | Generator pre image loss = {all_losses['all_losses_generator_pre'][-1]:.2f} | Pre to post cycle loss = {all_losses['all_losses_pre_to_post_cycle'][-1]:.2f} | Post to pre cycle loss = {all_losses['all_losses_post_to_pre_cycle'][-1]:.2f} | Discriminator pre real image loss = {all_losses['all_losses_discriminator_pre_real'][-1]:.2f} | Discriminator post real image loss = {all_losses['all_losses_discriminator_post_real'][-1]:.2f} | Discriminator pre synthetic image loss = {all_losses['all_losses_discriminator_pre_synthetic'][-1]:.2f} | Discriminator post synthetic image loss = {all_losses['all_losses_discriminator_post_synthetic'][-1]:.2f}")
+        
+    elif model.lower()=="attentiongan":
+        None
+        
+    else:
+        raise NotImplementedError("Model must be one of: Pix2Pix, CycleGAN or AttentionGAN")
+    
